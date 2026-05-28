ab2c54fee0c95d99c685cd456ff0c2758a3dfd2b
Ali Galvan <galbar@hotmail.es>
2026-05-28 15:22:12 -0600

    adding deserializable derive
    Updated Estadisticas struct to use u16 for counts and added serialization support.

--- tekla-back/src/models/estadisticas.rs
+++ tekla-back/src/models/estadisticas.rs
@@ -1,8 +1,10 @@
 use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+
 
 ///Modelo de errores por tecla
 ///
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Errores {
     pub tecla: char,
     pub cantidad: u16,
@@ -10,34 +12,14 @@ pub struct Errores {
 
 //Estadisticas de la leccion
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Estadisticas {
     pub tecla_actual: String,
-    pub posicion_actual: u32,
-    pub total_errores: u32,
+    pub posicion_actual: u16,
+    pub total_errores: u16,
     pub error_por_tecla: Vec<Errores>,
-    pub total_caracteres_leccion: u32,
-    pub caracteres_correctos: u32,
+    pub total_caracteres_leccion: u16,
+    pub caracteres_correctos: u16,
     pub tiempo_inicio: Option<DateTime<Utc>>,
     pub tiempo_fin: Option<DateTime<Utc>>,
-}
-
-
-impl Estadisticas{
-        pub fn new(
-        tecla_actual: String,
-        posicion_actual: u32,
-        total_caracteres_leccion: u32,
-    ) -> Self {
-        Estadisticas {
-            tecla_actual,
-            posicion_actual,
-            total_errores: 0,
-            error_por_tecla: Vec::new(),
-            total_caracteres_leccion,
-            caracteres_correctos: 0,
-            tiempo_inicio: Some(Utc::now()),
-            tiempo_fin: None,
-        }
-    }
 }
